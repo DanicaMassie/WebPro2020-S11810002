acefa63217080dad4a8b82f1c5cf1dafdf50c85d
DanicaMassie <S11810002@student.unklab.ac.id>
2020-11-11 13:14:49 +0800

    commited

---
+++ webprogramming/src/utils/person.js
@@ -0,0 +1,26 @@
+export const person = [
+    {
+        name: "Anj",
+        institution: "Unklab",
+        address: "Ratahan",
+        phone: "085xxxxxxx",
+    },
+    {
+        name: "Det",
+        institution: "Unsrat",
+        address: "Manado",
+        phone: "082xxxxxxx",
+    },
+    {
+        name: "Higgns",
+        institution: "Unima",
+        address: "Manado",
+        phone: "089xxxxxxx",
+    },
+    {
+        name: "Zack",
+        institution: "Unklab",
+        address: "Manado",
+        phone: "081xxxxxxx",
+    },
+    ];
