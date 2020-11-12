40e6c4c039b53896bb2c6262742673d71de2da22
DanicaMassie <69837325+DanicaMassie@users.noreply.github.com>
2020-11-12 14:07:36 +0800

    Add files via upload
    tugas

---
+++ Tugass/src/App.js
@@ -0,0 +1,12 @@
+import Container from './Components/container.js'
+import './stylesheet/style.min.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>react people!</h1>
+      <Container/>
+    </div>
+  );
+}
+
+export default App;
